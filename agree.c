0474c730ee8e8cb19708ca27678acad0f01d7713
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-07-20 13:46:55 +0000

    Thu, Jul 20, 2023, 2:46 PM +01:00

--- agree.c
+++ agree.c
@@ -10,7 +10,15 @@ int main(void)
 
     printf("Agreed.\n");
     }
+    else if (c = 'Y')
+    {
+        printf("Agreed.\n");
+    }
     else if (c == 'n')
+    {
+        printf("Not Agreed.\n");
+    }
+    else if (c == 'N')
     {
         printf("Not Agreed.\n");
     }
