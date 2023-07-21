4a67e47f89572a25018aa22ae8470d906917561f
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-07-21 16:11:45 +0000

    Fri, Jul 21, 2023, 5:11 PM +01:00

--- mario.c
+++ mario.c
@@ -3,15 +3,12 @@
 
 int main(void)
 {
-    int n = get_int("Size: ");
-    if (n < 1)
-    {
-        n = get_int("Size: ");
-        if (n < 1)
-        {
-            n 
-        }
-    }
+  int n;
+  do
+  {
+    n = get_int("Size: ");
+  }
+  while (n < 1);
 
     for (int i = 0; i < n; i++)
 {
