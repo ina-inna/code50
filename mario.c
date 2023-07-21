1f3abed9760445123aa8b5b48f458f562dbaf302
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-07-21 16:08:26 +0000

    Fri, Jul 21, 2023, 5:08 PM +01:00

--- mario.c
+++ mario.c
@@ -4,6 +4,11 @@
 int main(void)
 {
     int n = get_int("Size: ");
+    if (n < 1)
+    {
+        n = get_int("Size: ");
+    }
+
     for (int i = 0; i < n; i++)
 {
         for (int j = 0; j < n; j++)
@@ -12,4 +17,5 @@ int main(void)
 }
     printf("\n");
 }
+
 }
