7b4d7307cde5598df9cf0748a81d8294caffafc3
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-07-23 10:57:44 +0000

    Sun, Jul 23, 2023, 11:57 AM +01:00

--- population/population.c
+++ population/population.c
@@ -6,28 +6,27 @@ int main(void)
     // TODO: Prompt for start size
 
     int n;
-  do
-  {
-    n = get_int("Start size: ");
-  }
-  while (n < 9);
+    do
+    {
+        n = get_int("Start size: ");
+    }
+    while (n < 9);
 
     // TODO: Prompt for end size
     int j;
-  do
-  {
-    j = get_int("End size: ");
-  }
-  while (j < n);
-
+    do
+    {
+        j = get_int("End size: ");
+    }
+    while (j < n);
 
     // TODO: Calculate number of years until we reach threshold
-int num = 0;
-while (n < j)
-{
-    n = n + (n / 3) - (n / 4);
-    ++num;
-}
+    int num = 0;
+    while (n < j)
+    {
+        n = n + (n / 3) - (n / 4);
+        ++num;
+    }
     // TODO: Print number of years
     printf("Years: %i\n", num);
 }
