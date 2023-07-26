30b6e9e983b8b062b19db37a505c9ef0289709f0
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-07-26 16:18:14 +0000

    Wed, Jul 26, 2023, 5:18 PM +01:00

--- phonebook.c
+++ phonebook.c
@@ -5,8 +5,5 @@ int main(void)
  {
         string answer = get_string("Name: ");
         printf("%s!\n", answer);
-    }
-    {
-        int i = get_int("Age: ");
-        printf("%s\n", i);
+    
     }
