8c549dcd687fdb74550c48f5203913ae3ff7feee
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-07-28 15:17:26 +0000

    Fri, Jul 28, 2023, 4:17 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,27 +3,26 @@
 
 int main(void)
 {
-      // Get size of grid
-  int n;
-  do
-  {
-    n = get_int("Size: ");
-  }
-  while ( n < 1 || n > 8);
+    // Get size of grid
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1 || n > 8);
 
-  // Print grid of bricks
+    // Print grid of bricks
 
     for (int row = 0; row < n; row++)
-{
+    {
         for (int space = 0; space < n - row - 1; space++)
         {
-          printf(" ");
+            printf(" ");
         }
         for (int column = 0; column <= row; column++)
         {
-          printf("#");
+            printf("#");
         }
-          printf("\n");
-
-}
+        printf("\n");
+    }
 }
