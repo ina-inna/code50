99907f5838f0dd37b564572de4775eac5b8eaafe
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-07-28 14:56:16 +0000

    Fri, Jul 28, 2023, 3:56 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -15,6 +15,10 @@ int main(void)
 
     for (int row = 0; row < n; row++)
 {
+        for (int space = 0; space < n - row - 1; space++)
+        {
+          printf(" ");
+        }
         for (int column = 0; column <= row; column++)
 {
     printf("#");
