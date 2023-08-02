7905854c7cfe5447d2cf1514b4f1cd2f5acc18ed
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-02 21:48:54 +0000

    Wed, Aug 2, 2023, 10:48 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,4 +5,16 @@ int main(void)
 {
     long number = get_long("Number: ");
 
+// Number is ok, print VISA
+
+    if ()
+   {
+    printf("VISA\n");
+   }
+
+// Number is not ok, print Invalid
+    else
+    {
+     printf("Invalid\n");
+    }
 }
