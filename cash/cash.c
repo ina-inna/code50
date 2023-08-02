c7713ac67a0cae199b7a2f7302cdf33b9963b919
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-02 08:35:11 +0000

    Wed, Aug 2, 2023, 9:35 AM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -52,9 +52,9 @@ int get_cents(void)
 int calculate_quarters(int cents)
 {
     // TODO
-   int q = cents / 25;
+   int quarters = cents / 25;
 
-    return q;
+    return cents;
 }
 
 int calculate_dimes(int cents)
