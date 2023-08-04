ae2ff33a15756769b6ffc41da3c4c109ea7ef031
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-04 17:10:21 +0000

    Fri, Aug 4, 2023, 6:10 PM +01:00

--- length.c
+++ length.c
@@ -1,13 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main (void)
 {
-    string name = get_string("what's your name?");
+    string name = get_string("what's your name? ");
 
-    int n = 0
-    while (\name[n] != '\0')
-    {
-        n++
-    }
-    print("%i\n, n);
+    int n = strlen(name);
+    printf("%i\n", n);
+}
