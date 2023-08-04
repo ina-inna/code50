dc196130599157100a811c5a0b8b9ec023dd5f0f
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-04 17:17:24 +0000

    Fri, Aug 4, 2023, 6:17 PM +01:00

--- uppercase.c
+++ uppercase.c
@@ -10,7 +10,12 @@ int main(void)
     {
         if (s[i] >= 'a' && s[i] <= 'z')
         {
-            printf("%c)
+            printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf("%c", s[i]);
         }
     }
+    printf("\n");
 }
