2b3b2ce9772aa23cb7dba817e82a5960c8c60e38
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-06 14:59:30 +0000

    Sun, Aug 6, 2023, 3:59 PM +01:00

---
+++ triangle.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main()
+{
+    bool isTrue = true;
+    bool isFalse = false;
+}
+if (isTrue) {
+        // This block will execute because isTrue is true.
+    }
