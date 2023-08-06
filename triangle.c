bb1c5e9942f51fb8904c267c2621f2d273e34a53
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-06 15:09:48 +0000

    Sun, Aug 6, 2023, 4:09 PM +01:00

--- triangle.c
+++ triangle.c
@@ -2,14 +2,5 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int a,b,c;
-
-int main()
-{
-    bool isTrue = true;
-    bool isFalse = false;
-}
-if (isTrue) {
-        // This block will execute because isTrue is true.
-       for int triangle (a, b, c)
-    }
+int main(void);
+float valid_triangle(float x, float y, float z);
