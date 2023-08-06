2c1e063dba20088e7cc92fffe37091cb9f342193
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-06 13:24:23 +0000

    Sun, Aug 6, 2023, 2:24 PM +01:00

--- greet.c
+++ greet.c
@@ -3,5 +3,12 @@
 
 int main(int argc, string argv[])
 {
-    printf("hello, %s %s\n", argv[1], argv[2]);
+    if (argc == 2)
+    {
+    printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
 }
