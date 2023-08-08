00f91de439050e7aa42c0ca647eb354171fb42a7
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-08 13:58:29 +0000

    Tue, Aug 8, 2023, 2:58 PM +01:00

--- array.c
+++ array.c
@@ -11,9 +11,7 @@ int main(void)
     while (length < 1);
 
     int array[length];
-    array[0] = 1;
     for (int i = 0; i < length; i++)
-    array[0] = 1;
 
     {
             printf("%i\n", array[length]);
