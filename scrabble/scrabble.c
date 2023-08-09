4565cd542a7cc4fbe59663459a16f8fdeb0cb5b0
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-09 15:38:05 +0000

    Wed, Aug 9, 2023, 4:38 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,19 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins\n");
+    }
+    else if (score1 == score2)
+    {
+        printf("Tie\n");
+    }
+    else
+    {
+        printf("Player 2 wins\n");
+    }
+
 }
 
 int compute_score(string word)
