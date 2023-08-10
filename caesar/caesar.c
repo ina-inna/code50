6d04a2a2a3eaf03021319bc658ad5d528adea126
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-10 14:23:09 +0000

    Thu, Aug 10, 2023, 3:23 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    // let user input just 1 number
+
+    // prompt user to type a phrase
+
+    // 
 
 }
