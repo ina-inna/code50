19687fca36f902c5c97e721c4a5453ccf3e0a1fc
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-10 14:28:11 +0000

    Thu, Aug 10, 2023, 3:28 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,9 +4,19 @@
 int main(int argc, string argv[])
 {
     // let user input just 1 number
+    if (argc < 1;
+    {
+        printf("%i\n", argv);
+        return 0
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");\
+        return 1
+    }
 
     // prompt user to type a phrase
 
-    // 
+    //
 
 }
