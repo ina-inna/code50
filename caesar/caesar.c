f4b3c0f7fc24ab111b1877b9f57900040919a069
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-10 14:58:38 +0000

    Thu, Aug 10, 2023, 3:58 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -14,14 +14,16 @@ int main(int argc, char *argv[])
         printf("Usage: ./caesar key\n");\
         return 1;
     }
-    // only digits
+    // check if argv[1] is only digits
     bool only_digits(argv[1]);
     int length = strlen(argv[1]);
-    
 
+    // convert argv[1] from a string to an int
 
     // prompt user to type a phrase
 
-    //
+    // for each character in the phrase
+
+        // rotate the character if it's a letter
 
 }
