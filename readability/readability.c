9596aee4636a4843b34639c5cb0622f481ed2763
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-12 11:37:16 +0000

    Sat, Aug 12, 2023, 12:37 PM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    // prompt user for an input
+
+    // count and print letters
+
+    // count and print words
+
+    // count and print sentences
+
+    // compute a grade using the given formula
 
 }
