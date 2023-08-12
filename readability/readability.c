14a591afef9e3fcd31778d59cc29583742fd5dd9
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-12 18:09:45 +0000

    Sat, Aug 12, 2023, 7:09 PM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int count_letters(string text);
 int count_words(string text);
@@ -38,7 +39,7 @@ int main(void)
         }
         else
         {
-            printf("Grade %d\n", round(index));
+            printf("Grade %f\n", round(index));
         }
 }
 
