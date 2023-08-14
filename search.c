6921201bc2882299903fe176d425a26c48614328
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-14 12:09:17 +0000

    Mon, Aug 14, 2023, 1:09 PM +01:00

--- search.c
+++ search.c
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
-    int n = get_int("Number: ");
+    string s = get_string("string: ")
     for (int i = 0; i < 7; i++)
     {
         if (numbers[i] == n)
