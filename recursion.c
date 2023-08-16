63924edd07f9f0a4eadd2c8e4ef03cf9766bc3ee
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-16 13:10:12 +0000

    Wed, Aug 16, 2023, 2:10 PM +01:00

--- recursion.c
+++ recursion.c
@@ -3,14 +3,17 @@
 #include <string.h>
 
 void draw(int n);
+void draww(int m);
 
 int main(void)
 {
     int height = get_int("Height: ");
     draw(height);
+    draww(height);
 }
 
 void draw(int n)
+
 {
    if (n <= 0)
     {
@@ -24,4 +27,17 @@ void draw(int n)
     printf("\n");
     draw(n - 1);
 
+}
+void draww(int m)
+{
+   if (m <= 0)
+    {
+        return;
+    }
+    draw1(m - 1);
+    for (int i = 0; i < m; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
