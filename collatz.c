2f83daca2823efbcf070049853211bff918eda69
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-16 16:11:32 +0000

    Wed, Aug 16, 2023, 5:11 PM +01:00

---
+++ collatz.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int collatz(int n);
+
+int main(void)
+{
+    int number = get_int("Number: ");
+    collatz(number);
+}
+
+
+int collatz(int n)
+{
+    if n
+
+
+}
