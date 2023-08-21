3fa995198394bb7d33cdbdd97b259c3a370383d2
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-21 20:35:11 +0000

    Mon, Aug 21, 2023, 11:35 PM +03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -72,6 +72,7 @@ bool vote(string name)
     // if name matches, then +1 vote to the candidate with the same name
 
     int i = 0;
+    
     if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes = 1;
