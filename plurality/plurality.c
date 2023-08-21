ccaebc227d87b3366b02ae69742642ea388e6753
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-21 16:00:27 +0000

    Mon, Aug 21, 2023, 7:00 PM +03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,10 +71,10 @@ bool vote(string name)
 
     // if name matches, then vote 1+vote
 
-    //  if (strcmp(candidate[i], name) == 0)
-    //     {
-    //         return 1 + vote(name);
-    //     }
+     if (strcmp(candidate[i], name) == 0)
+        {
+            return 1 + vote(name);
+        }
 
     // // if not, then
 
