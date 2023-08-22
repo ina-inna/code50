a20ac84ef1145f11b08d9b526d63385b4b66e3bc
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-22 21:42:12 +0000

    Wed, Aug 23, 2023, 12:42 AM +03:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -33,8 +33,7 @@ int convert(string input)
 
 
         input[n - 1] = '\0';
-        char last_char = input[n - 2];
-        int last = (last_char - '0') * 10;
+        int last = (input[n - 2] - '0') * 10;
 
     return num + last;
 
