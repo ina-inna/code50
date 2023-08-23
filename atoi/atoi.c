15369a9b3fcc7a6a5ce7670299278ae435bccd08
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-23 11:09:54 +0000

    Wed, Aug 23, 2023, 2:09 PM +03:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -29,31 +29,39 @@ int convert(string input)
 {
     // TODO
 
-    // if it's a single digit string:
-return convert_helper(input, strlen(input))
+    // base case;
+    if (strlen(input) == 0)
+    {
+        return 0;
+    }
+    int num = (input[n - 1] - '0');
+    // shortening the string
+    input[n - 1] = '\0';
 
+    int remainingValue = convert(input);
+    int result = remainingValue * 10 + num;
 
-
+    return result;
 
 }
 
-int convert_helper(string input, int original_length)
-{
-int n = strlen(input);
-int position = original_length - n;
-    // if (n >= 0 && n <= 1)
-    // {
-    //     int num = input[n - 1] - '0';
-    //     return num;
-    // }
-    // // if it's >1 digit string
-    // else
-    // {
-    int num = input[n - 1] - '0';   // define last digit
+// int convert_helper(string input, int original_length)
+// {
+// int n = strlen(input);
+// int position = original_length - n;
+//     // if (n >= 0 && n <= 1)
+//     // {
+//     //     int num = input[n - 1] - '0';
+//     //     return num;
+//     // }
+//     // // if it's >1 digit string
+//     // else
+//     // {
+//     int num = input[n - 1] - '0';   // define last digit
 
 
-        input[n - 1] = '\0';        // truncate the string by 1
-        // int last = (input[n - 2] - '0') * 10;       // value of the n - 2 digit
-        // int sum = num + last;
-        return num + (input[n - 2] - '0') * 10;
-}
+//         input[n - 1] = '\0';        // truncate the string by 1
+//         // int last = (input[n - 2] - '0') * 10;       // value of the n - 2 digit
+//         // int sum = num + last;
+//         return num + (input[n - 2] - '0') * 10;
+// }
