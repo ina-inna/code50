e7b6c18eeb40ef6a16c5f113b865dca4986f7c74
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-23 10:18:10 +0000

    Wed, Aug 23, 2023, 1:18 PM +03:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -44,8 +44,8 @@ int convert(string input)
 
 
         input[n - 1] = '\0';        // truncate the string by 1
-        int last = (input[n - 2] - '0');       // value of the n - 2 digit
-        int sum = num + last * 10;
+        int last = (input[n - 2] - '0') * 10;       // value of the n - 2 digit
+        int sum = num + last;
         return sum + convert(input);
     }
 
