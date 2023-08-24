939dfecbabb2ddb8b86a2819a3fec35439073d94
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-24 09:27:08 +0000

    Thu, Aug 24, 2023, 12:27 PM +03:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -75,34 +75,34 @@ int main(void)
 void add_items(void)
 {
     menu[0].item = "Burger";
-    menu[0].price = $9.50;
+    menu[0].price = 9.50;
 
     menu[1].item = "Vegan Burger";
-    menu[1].price = $11.00;
+    menu[1].price = 11.00;
 
     menu[2].item = "Hot Dog";
-    menu[2].price = $5.00;
+    menu[2].price = 5.00;
 
     menu[3].item = "Cheese Dog";
-    menu[3].price = $7.00;
+    menu[3].price = 7.00;
 
     menu[4].item = "Fries";
-    menu[4].price = $5.00;
+    menu[4].price = 5.00;
 
     menu[5].item = "Cheese Fries";
-    menu[5].price = $6.00;
+    menu[5].price = 6.00;
 
     menu[6].item = "Cold Pressed Juice";
-    menu[6].price = $7.00;
+    menu[6].price = 7.00;
 
     menu[7].item = "Cold Brew";
-    menu[7].price = $3.00;
+    menu[7].price = 3.00;
 
     menu[8].item = "Water";
-    menu[8].price = $2.00;
+    menu[8].price = 2.00;
 
     menu[9].item = "Soda";
-    menu[9].price = $2.00;
+    menu[9].price = 2.00;
 
     return;
 }
