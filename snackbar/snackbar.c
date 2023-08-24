1d4abdf3405fec1f2cb252d05dd3aa21e312dbb0
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-24 09:57:11 +0000

    Thu, Aug 24, 2023, 12:57 PM +03:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -112,7 +112,7 @@ float get_cost(string item)
 {
     // check if the item is in the struct
     for (int i = 0, i < NUM_ITEMS, i++)
-    if item = menu[i].item
+    if strcmp (item = menu[i].item
 
     // if yes, return the cost
 
