adcbbb52e74c5fe2ebe798c68c7fafc74d266c97
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-24 20:29:40 +0000

    Thu, Aug 24, 2023, 11:29 PM +03:00

--- temps/temps.c
+++ temps/temps.c
@@ -22,7 +22,8 @@ int compare(const void *a, const void *b);
 
 int main(void)
 {
-    struct avg_temp temps
+    struct avg_temp temps [NUM_CITIES];
+    
     temps[0].city = "Austin";
     temps[0].temp = 97;
 
