b78b4ebcb9878220a7b7998c1e86691fa9cdccfd
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-26 21:03:35 +0000

    Sun, Aug 27, 2023, 12:03 AM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -144,7 +144,6 @@ bool vote(int voter, int rank, string name)
             candidates[i].votes = candidates[i].votes + 1;
             return true;
         }
-        printf("%i\n", preferences[voter][rank]);
     }
     return false;
     }
