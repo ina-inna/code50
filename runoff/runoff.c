a3bba6a5ef92ce56c9fb1c925300dba4e3f41dc7
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-29 12:25:04 +0000

    Tue, Aug 29, 2023, 3:25 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -144,7 +144,7 @@ bool vote(int voter, int rank, string name)
 
                     // if yes, return the index and +1 vote to the candidate with the same name
                     {
-                        preferences[voter][rank] = i;
+                        int index = i;
                         // candidates[i].votes = candidates[i].votes + 1;
                         return true;
                     }
