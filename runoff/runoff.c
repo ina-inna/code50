bd11921b90307979e0e7402aa1c54399544454ab
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-29 12:12:38 +0000

    Tue, Aug 29, 2023, 3:12 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -148,6 +148,7 @@ bool vote(int voter, int rank, string name)
                         // candidates[i].votes = candidates[i].votes + 1;
                         return true;
                     }
+                    rank++;
                  }
 
 
