fc99d5388dd56b75b5fd18010bd130840800b456
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-08-30 15:25:17 +0000

    Wed, Aug 30, 2023, 6:25 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -183,7 +183,7 @@ bool print_winner(void)
     for (int i = 0; i < candidate_count; i++)
 
     {
-        if (candidates[i].votes > )
+        if (candidates[i].votes > (sum =+))
     }
     return false;
 }
