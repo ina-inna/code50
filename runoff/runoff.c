0e39703669d4d8be9512beedf5f19449469ac898
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-09-04 22:49:18 +0000

    Tue, Sep 5, 2023, 1:49 AM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -203,11 +203,11 @@ int find_min(void)
 {
     // TODO
     // while candidate not eliminated compare and find the minimum vote total
-    int min = candidates[0].votes;
+    int min;
     for (int i = 1; i < candidate_count; i++)
     {
 
-        if (candidates[i].eliminated == false && candidates[i].votes < min)
+        if (candidates[i].eliminated == false && candidates[i].votes < candidates[0].votes)
         {
             min = candidates[i].votes;
         }
