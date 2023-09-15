c1983f5c8ba962ed7d12b07d61365ab7eb624add
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-09-15 18:21:43 +0000

    Fri, Sep 15, 2023, 9:21 PM +03:00

--- pdf.c
+++ pdf.c
@@ -24,14 +24,17 @@ int main(int argc, string argv[])
     uint8_t buffer[4];
     uint8_t signature[] = {37, 80, 68, 70};
     fread(buffer, 1, 4, file);
+    fclose(file);
     for (int i = 0; i < 4; i++)
     {
         if (buffer[i] != signature [i])
         {
             printf("Likely not a pdf.");
+            fclose(file)
             return 0;
         }
     }
     printf("Likely a pdf.\n")
+    fclose(file)
     return 0;
 }
