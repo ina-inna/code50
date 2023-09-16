fc43b0929d691cdf11a19f75147251a2e49e8fcd
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-09-16 21:26:03 +0000

    Sat, Sep 16, 2023, 10:26 PM +01:00

--- pointers.c
+++ pointers.c
@@ -4,12 +4,18 @@
 int main(void)
 
 {
-int a = 28;
-int b = 50;
-int *c = &a;
+int  a = 28;
+int  b = 50;
+int *c = 50;
 
 *c = 14;
 c = &b;
 *c = 25;
+
+string  a = "1234";
+string* b = a;
+
+
+printf("%s %s", a, *b);
 
 }
