7988194554283035229ebad3c30e20149acc028f
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-09-21 16:43:01 +0000

    Thu, Sep 21, 2023, 5:43 PM +01:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 
     // modifying biHeight
     long newHeight = -bi.biHeight;
-    
+    bi.biHeight = newHeight;
 
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
