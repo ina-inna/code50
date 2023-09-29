a0e16613ad2544f425e0cbafd718df5af9a1b0df
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-09-29 17:53:06 +0000

    Fri, Sep 29, 2023, 6:53 PM +01:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -4,15 +4,19 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    height = 0;
-    width = 0;
-    RGBTRIPLE pixel = image[height][width];
-
-    if (pixel.rgbtBlue == 255 && pixel.rgbtGreen == 255 && pixel.rgbtRed == 255)
+    for (int i = 0; i < height; i++)
     {
-        pixel.rgbtBlue = 0;
-        pixel.rgbtGreen = 0;
-        pixel.rgbtRed = 255;
+        for (int j = 0; j < width; j++)
+        {
+        RGBTRIPLE pixel = image[height][width];
+
+        if (pixel.rgbtBlue == 0 && pixel.rgbtGreen == 0 && pixel.rgbtRed == 0)
+            {
+                pixel.rgbtBlue = 0;
+                pixel.rgbtGreen = 0;
+                pixel.rgbtRed = 255;
+            }
+    }
     }
 
 }
