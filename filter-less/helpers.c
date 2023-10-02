0dc151372b4b0e07383a500e92df7e926cea3f1b
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-02 14:05:15 +0000

    Mon, Oct 2, 2023, 3:05 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -52,6 +52,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             {
                     int left2right = width - j;
                     RGBTRIPLE pixel0 = image[i][j];
+                    RGBTRIPLE pixel00 = image[i][left2right];
                     RGBTRIPLE *pixel = &image[i][j];
                     RGBTRIPLE *pixel2 = &image[i][left2right];
 
