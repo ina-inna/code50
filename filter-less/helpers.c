8cf4a18c680f45f94d5876a7dbc4b573301bff57
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-02 15:37:34 +0000

    Mon, Oct 2, 2023, 4:37 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -32,19 +32,20 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             {
-                image[i][j].rgbtRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+                RGBTRIPLE pixel = image[i][j];
+                image[i][j].rgbtRed = .393 * pixel.rgbtRed + .769 * pixel.rgbtGreen + .189 * pixel.rgbtBlue;
                 if (image[i][j].rgbtRed > 255)
                 {
                     image[i][j].rgbtRed = 255;
                 }
 
-                image[i][j].rgbtGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+                image[i][j].rgbtGreen = .349 * pixel.rgbtRed + .686 * pixel.rgbtGreen + .168 * pixel.rgbtBlue;
 
                  if (image[i][j].rgbtGreen > 255)
                 {
                     image[i][j].rgbtGreen = 255;
                 }
-                image[i][j].rgbtBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+                image[i][j].rgbtBlue = .272 * pixel.rgbtRed + .534 * pixel.rgbtGreen + .131 * pixel.rgbtBlue;
 
                  if (image[i][j].rgbtBlue > 255)
                 {
