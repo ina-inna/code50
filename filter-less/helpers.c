fc531bdd1c7b568f4036d95e963a735b88f78883
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-02 11:03:20 +0000

    Mon, Oct 2, 2023, 12:03 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -14,14 +14,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             {
-                image[i][j].rgbtBlue = 0;
-                image[i][j].rgbtGreen = 0;
-                image[i][j].rgbtRed = 255;
+                image[i][j].rgbtBlue = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+                image[i][j].rgbtGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+                image[i][j].rgbtRed = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;;
             }
         }
     }
-
-
     return;
 }
 
