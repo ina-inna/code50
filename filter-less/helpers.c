b1a05ae7c348c9d9502c0a27b722680123f9b58e
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-03 11:00:30 +0000

    Tue, Oct 3, 2023, 12:00 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -159,13 +159,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
 
 
-                for (int i = width; i < height; i++)
+                for (int i = 1; i < height; i++)
 
         {
             int j = width;
-            int blur1 = round((image[i][j].rgbtBlue + image[i][j+1].rgbtBlue + image[i-1][j].rgbtBlue + image[i-1][j+1].rgbtBlue + image[i+1][j].rgbtBlue + image[i+1][j+1].rgbtBlue) / 6);
-            int blur2 = round((image[i][j].rgbtGreen + image[i][j+1].rgbtGreen + image[i-1][j].rgbtGreen + image[i-1][j+1].rgbtGreen + image[i+1][j].rgbtGreen + image[i+1][j+1].rgbtGreen) / 6);
-            int blur3 = round((image[i][j].rgbtRed + image[i][j+1].rgbtRed + image[i-1][j].rgbtRed + image[i-1][j+1].rgbtRed + image[i+1][j].rgbtRed + image[i+1][j+1].rgbtRed) / 6);
+            int blur1 = round((image[i][j].rgbtBlue + image[i][j-1].rgbtBlue + image[i-1][j].rgbtBlue + image[i-1][j-1].rgbtBlue + image[i+1][j].rgbtBlue + image[i+1][j-1].rgbtBlue) / 6);
+            int blur2 = round((image[i][j].rgbtGreen + image[i][j-1].rgbtGreen + image[i-1][j].rgbtGreen + image[i-1][j-1].rgbtGreen + image[i+1][j].rgbtGreen + image[i+1][j-1].rgbtGreen) / 6);
+            int blur3 = round((image[i][j].rgbtRed + image[i][j-1].rgbtRed + image[i-1][j].rgbtRed + image[i-1][j-1].rgbtRed + image[i+1][j].rgbtRed + image[i+1][j-1].rgbtRed) / 6);
 
 
                 image[i][j].rgbtBlue = blur1;
