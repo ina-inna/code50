fba780d3f4b199ec94e5493eb807190f1bb4662a
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-03 12:34:47 +0000

    Tue, Oct 3, 2023, 1:34 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -100,6 +100,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 1; j < width; j++)
         {
 
+
+
+
             {
                int blur1 = round((image[i][j].rgbtBlue + image[i][j-1].rgbtBlue + image[i][j+1].rgbtBlue + image[i-1][j-1].rgbtBlue + image[i-1][j].rgbtBlue + image[i-1][j+1].rgbtBlue + image[i+1][j-1].rgbtBlue + image[i+1][j].rgbtBlue + image[i+1][j+1].rgbtBlue) / 9.0);
                int blur2 = round((image[i][j].rgbtGreen + image[i][j-1].rgbtGreen + image[i][j+1].rgbtGreen + image[i-1][j-1].rgbtGreen + image[i-1][j].rgbtGreen + image[i-1][j+1].rgbtGreen + image[i+1][j-1].rgbtGreen + image[i+1][j].rgbtGreen + image[i+1][j+1].rgbtGreen) / 9.0);
