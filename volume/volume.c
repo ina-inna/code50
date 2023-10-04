78741f12876dac3009710a369f86670002ddcfc4
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-04 16:25:50 +0000

    Wed, Oct 4, 2023, 5:25 PM +01:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,6 +43,10 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
 
+    int16_t buffer;
+
+    
+
     // Close files
     fclose(input);
     fclose(output);
