2e025218ebb9a504ffff36512189253e14d07bc9
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-05 16:29:29 +0000

    Thu, Oct 5, 2023, 5:29 PM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
     int origin = SEEK_END;
 
         int16_t audioData;
-        while (fseek(inptr, offset, origin) == position)
+        while (fseek(inptr, offset, origin) != position)
 
         // write blocks to outfile
         {
