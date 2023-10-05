c0c55b4671e36ceb636ba04aecd162ed45ebe2ce
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-05 14:57:25 +0000

    Thu, Oct 5, 2023, 3:57 PM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -67,6 +67,7 @@ int main(int argc, char *argv[])
     // Write reversed audio to file
     // TODO #8
     int num = header.subchunk2Size / get_block_size;
+    int16_t buffer;
 
      for (int i = num; i < 0; i--)
     {
