94472a4ae3e7c6434182df3029209dce9897319c
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-05 14:30:08 +0000

    Thu, Oct 5, 2023, 3:30 PM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -62,6 +62,7 @@ int main(int argc, char *argv[])
 
     // Use get_block_size to calculate size of block
     // TODO #7
+    get_block_size(header);
 
     // Write reversed audio to file
     // TODO #8
