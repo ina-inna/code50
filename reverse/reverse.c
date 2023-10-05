93a5dc1919531c69d0266731111db036cf7d7a77
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-05 17:22:45 +0000

    Thu, Oct 5, 2023, 6:22 PM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -81,10 +81,36 @@ int main(int argc, char *argv[])
 
     // Write reversed audio to file
     // TODO #8
-   // long int offset = -block_size;
-    //int origin = SEEK_END;
 
-   
+     fseek(inptr, 0, SEEK_END);
+    long int file_size = ftell(inptr);
+
+    int16_t audioData;
+    // find file_size
+    int current_position = file_size;
+
+        // write blocks to outfile
+
+        while (current_position > position)
+        {
+            fseek(inptr, current_position - block_size, SEEK_SET);
+            if (fread(&audioData, block_size, 1, inptr) == 1)
+            {
+                    int16_t buffer = audioData;
+                    fwrite(&buffer, block_size, 1, outptr);
+            }
+
+            else
+            {
+                printf("Error when reading file %s\n", input);
+            }
+
+            current_position -= block_size;
+
+        }
+
+
+
     // close infile
     fclose(inptr);
 
