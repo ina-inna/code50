33e28345c8cd7e78b9507c7df9243daad8e265d6
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-05 20:55:28 +0000

    Thu, Oct 5, 2023, 9:55 PM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
     {
         long int file_size = ftell(inptr);
 
-        int8_t audioData;
+        int16_t audioData;
         // find file_size
         long int current_position = file_size;
         printf("%li\n", current_position);
@@ -93,10 +93,10 @@ int main(int argc, char *argv[])
             while (current_position > position)
             {
                 fseek(inptr, -block_size, SEEK_CUR);
-                if (fread(&audioData, block_size, 1, inptr) == 1)
+                if (fread(&audioData, 2, 1, inptr) == 1)
                 {
-                        int8_t buffer = audioData;
-                        fwrite(&buffer, block_size, 1, outptr);
+                        int16_t buffer = audioData;
+                        fwrite(&buffer, 2, 1, outptr);
                 }
 
                 else
