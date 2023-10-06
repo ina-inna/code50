36e8971d47ae4c13c92ec652faf9421b3b43d671
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-06 10:37:12 +0000

    Fri, Oct 6, 2023, 11:37 AM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-      // remember filenames
+    // remember filenames
     char *input = argv[1];
     char *output = argv[2];
 
@@ -33,14 +33,14 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-
     // Read header
     // DONE #3
     WAVHEADER header;
     fread(&header, sizeof(WAVHEADER), 1, inptr);
 
     position = ftell(inptr);
-    if (position == -1L) {
+    if (position == -1L)
+    {
         printf("Error getting file %s position\n", input);
         return 3;
     }
@@ -48,8 +48,7 @@ int main(int argc, char *argv[])
 
     // Use check_format to ensure WAV format
     // DONE #4
-   int result = check_format(header);
-
+    int result = check_format(header);
 
     // Open output file for writing
     // DONE #5
@@ -62,12 +61,10 @@ int main(int argc, char *argv[])
         return 3;
     }
 
-
     // Write header to file
     // Done#6
 
     fwrite(&header, sizeof(WAVHEADER), 1, outptr);
-
 
     // Use get_block_size to calculate size of block
     // Done #7
@@ -100,35 +97,28 @@ int main(int argc, char *argv[])
                         fwrite(&buffer, block_size, 1, outptr);
                     }
 
-                    else
-                    {
-                        printf("Error when reading file %s\n", input);
-                    }
-                    fseek(inptr, -2*block_size, SEEK_CUR);
+                else
+                {
+                    printf("Error when reading file %s\n", input);
                 }
-
-
-                // close infile
-                fclose(inptr);
-
-                // close outfile
-                fclose(outptr);
-
-                return 0;
+                fseek(inptr, -2 * block_size, SEEK_CUR);
             }
 
+            // close infile
+            fclose(inptr);
+
+            // close outfile
+            fclose(outptr);
+
+            return 0;
+        }
 
         else
 
-            {
-                printf("Error when reaching end of file %s\n", input);
-            }
-
+        {
+            printf("Error when reaching end of file %s\n", input);
+        }
     }
-
-
-
-
 }
 
 int check_format(WAVHEADER header)
@@ -137,7 +127,7 @@ int check_format(WAVHEADER header)
 
     if (header.format[0] == 'W' && header.format[1] == 'A' && header.format[2] == 'V' && header.format[3] == 'E')
     {
-    return 0;
+        return 0;
     }
 
     else
@@ -149,7 +139,7 @@ int check_format(WAVHEADER header)
 int get_block_size(WAVHEADER header)
 {
     // TODO #7
-    int block_size = ((header.bitsPerSample/8) * header.numChannels);
+    int block_size = ((header.bitsPerSample / 8) * header.numChannels);
     printf("%i %i %i\n", block_size, header.bitsPerSample, header.numChannels);
 
     return block_size;
