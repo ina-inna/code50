4196bd0f26f0b113a86844dff1aca768b63509d7
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-09 17:52:51 +0000

    Mon, Oct 9, 2023, 6:52 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    // check the number of command lines
+
+
+    // check the n
+
 
 }
