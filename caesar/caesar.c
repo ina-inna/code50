01b955f168f2187cdb45d8b5aa1aabd7e9baaf0b
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-10 13:49:22 +0000

    Tue, Oct 10, 2023, 2:49 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -71,7 +71,7 @@ char rotate(char letter, int number)
 {
     if (letter >= 'A' && letter <= 'Z')
         {
-            letter += number;
+            letter = ;
             //letter = (letter) % 26;
         }
     else if (letter >= 'a' && letter <= 'z')
