8a8dd9114e515b9088b9d8e2870882686c5f9622
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-10 14:22:00 +0000

    Tue, Oct 10, 2023, 3:22 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,20 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+int main(void) 
 {
-    long number = get_long("Number: ");
-
-// Number is ok, print VISA
-
-    if (number % 10 == 0)
-   {
-    printf("VISA\n");
-   }
-
-// Number is not ok, print Invalid
-    else
-    {
-     printf("Invalid\n");
-    }
+    
 }
