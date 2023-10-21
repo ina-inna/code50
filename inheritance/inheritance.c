9991497d3cd60c2392b0cac8f3e30d4c5870356b
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-21 19:05:08 +0000

    Sat, Oct 21, 2023, 8:05 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -108,18 +108,19 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p == NULL)
-    {
-        return;
-    }
+    while (p != NULL)
 
     // TODO: Free parents recursively
+   {
+
     free(p->parents[0]);
     free(p->parents[1]);
 
+   }
+
 
     // TODO: Free child
-    
+
 
 }
 
