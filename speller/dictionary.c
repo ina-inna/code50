c152c6f485297e01b0c2e47ad7d3e0816679d6ee
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-25 14:22:29 +0000

    Wed, Oct 25, 2023, 3:22 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -76,7 +76,7 @@ bool load(const char *dictionary)
 
             // insert node into hash table at that location -> word(hash number) add to a table(hash number)
 
-            new_node->next = table[location];
+            new_node->next = table[location]->next;
             table[location] = new_node;
 
             number_of_words++;
