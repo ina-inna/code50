351a2c326579c21857441d92f70f8334245d41e3
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-28 12:03:15 +0000

    Sat, Oct 28, 2023, 1:03 PM +01:00

--- bank/figlet/figlet.py
+++ bank/figlet/figlet.py
@@ -2,11 +2,11 @@ from pyfiglet import Figlet
 
 figlet = Figlet()
 figlet.getFonts()
-figlet.setFont(font=sys.argv[1])
 
 
 if len(sys.argv) == 0:
         text = input ("Input: ")
+        figlet.setFont(font=sys.argv[1])
         print(figlet.renderText(text))
 
 else:
