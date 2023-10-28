b96cb536407bcf7f2215592b30bab68205b901e0
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-28 12:14:06 +0000

    Sat, Oct 28, 2023, 1:14 PM +01:00

--- bank/figlet/figlet.py
+++ bank/figlet/figlet.py
@@ -11,9 +11,12 @@ if len(sys.argv) == 0:
         figlet.setFont(font=random(list))
         print(figlet.renderText(text))
 elif len(sys.argv) == 2:
-        if sys.argv[0] != "-f" or "--font":
-            sys.exit("Invalid usage")
-        elif sys.argv[1] != 
+        if sys.argv[0] != "-f" or "--font" and sys.argv[1] in list:
+                sys.exit("Invalid usage")
+        else:
+                text = input ("Input: ")
+                figlet.setFont(font=sys.argv[1])
+                print(figlet.renderText(text))
 
 else:
-         sys.exit("Invalid usage")
+        sys.exit("Invalid usage")
