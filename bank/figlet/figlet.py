8460a35eb400e8d3445b31a62e52fb9e124b60e7
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-28 12:07:13 +0000

    Sat, Oct 28, 2023, 1:07 PM +01:00

--- bank/figlet/figlet.py
+++ bank/figlet/figlet.py
@@ -10,6 +10,8 @@ if len(sys.argv) == 0:
         text = input ("Input: ")
         figlet.setFont(font=random(list))
         print(figlet.renderText(text))
+elif len(sys.argv) == 2:
+        if sys.argv[0]
 
 else:
          sys.exit("Invalid usage")
