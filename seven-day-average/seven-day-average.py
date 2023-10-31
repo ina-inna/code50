d1261e504b596dafcaaf04ec882d725c76d86fbf
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-31 19:10:16 +0000

    Tue, Oct 31, 2023, 7:10 PM +00:00

--- seven-day-average/seven-day-average.py
+++ seven-day-average/seven-day-average.py
@@ -37,6 +37,9 @@ def calculate(reader):
     new_cases = {}
     data = list(reader)
 
+    previous_cases = {}
+        
+
     for row in reversed(data):
         state = row['state']
         cases = row['cases']
