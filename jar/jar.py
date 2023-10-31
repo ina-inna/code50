08be380ad967613b23edc0ae28e1ffe08db4dc7d
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-10-31 15:20:38 +0000

    Tue, Oct 31, 2023, 3:20 PM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,6 +1,7 @@
 class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
+        self.cookies = 0
         if self.capacity < 0:
             raise ValueError("Not eligible")
 
@@ -9,15 +10,19 @@ class Jar:
         return 🍪
 
     def deposit(self, n):
-
+        if self.cookies > self.capacity:
+            raise ValueError("Not eligible")
+        self.cookies += n
 
     def withdraw(self, n):
-        ...
+        self.cookies -= n
+        if self.cookies < 0:
+            raise ValueError("Not eligible")
 
     @property
     def capacity(self):
-        ...
+        return self.capacity
 
     @property
     def size(self):
-        ...
+        return self.cookies
