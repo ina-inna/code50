cac5ad446214a7c66297117717d8ac5d98faa2fb
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-01 11:39:27 +0000

    Wed, Nov 1, 2023, 11:39 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,7 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    with open(sys.argv[1]) as file
+    with open(sys.argv[1]) as file:
         file_reader = csv.DictReader(sys.argv[1])
     # Store each team as a dictionary, convert rating into an integer
         football_teams = {}
