806230eb861d577923b800bf2cb47592cf26c1be
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-01 17:15:46 +0000

    Wed, Nov 1, 2023, 5:15 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,9 +1,8 @@
 
-while True:
+n = input("Height:\n")
+n = int(n)
+if n < 1 or n > 8:
     n = input("Height:\n")
-    n = int(n)
-    if n < 1 or n > 8:
-         n = input("Height:\n")
 
 for row in range (n):
     for space in range (n-1, n, -1):
