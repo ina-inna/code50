6c005df517c39cd4ce5c3d53982dbc19f5fc76d2
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-01 17:51:07 +0000

    Wed, Nov 1, 2023, 5:51 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,8 +1,7 @@
 n = input("Height: ")
-if int(n) < 1 or int(n) > 8:
+if n.isdigit() and int(n) < 1 or int(n) > 8:
     n = input("Height: ")
-else:
-    n = input("Height: ")
+
 for row in range (n+1):
     print(" "*(n - row) + "#"*row + " " + "#" * row)
 
