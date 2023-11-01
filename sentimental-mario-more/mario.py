a2e8fb812e0cf3cd9d3738c56a41e63889ad43de
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-01 18:10:38 +0000

    Wed, Nov 1, 2023, 6:10 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,11 +1,13 @@
-n = input("Height: ")
-if not n.isdigit():
+while True:
     n = input("Height: ")
-elif int(n) < 1 or int(n) > 8:
-    n = input("Height: ")
-elif int(n) >= 1 or int(n) <= 8:
-    n = int(n)
-    for row in range(n):
-        print(" " * (n - row - 1) + "#" * (row + 1) + "  " + "#" * (row + 1))
+    if not n.isdigit():
+        n = input("Height: ")
+    elif int(n) >= 1 or int(n) <= 8:
+        n = int(n)
         break
+    elif int(n) < 1 or int(n) > 8:
+       n = input("Height: ")
+
+for row in range(n):
+    print(" " * (n - row - 1) + "#" * (row + 1) + "  " + "#" * (row + 1))
 
