c970a2cc07e76485cf35c3b17b9674127fbb0d1f
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-01 17:07:09 +0000

    Wed, Nov 1, 2023, 5:07 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,7 +1,10 @@
 
 while answer < 1 or answer > 8:
     answer = input("Height:\n")
+n = int(answer)
 
-for row in range of int(answer):
-    
+for row in range of n:
+    for space in range (n - row - 1)
+        print(" ")
+    for block in range 
     print(f"hello, {answer}")
