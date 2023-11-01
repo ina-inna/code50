7163d15064a7db3d9332335249fd4bdb80506fb7
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-01 17:24:10 +0000

    Wed, Nov 1, 2023, 5:24 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -5,7 +5,7 @@ if n < 1 or n > 8:
     n = input("Height:\n")
 
 for row in range (n):
-    for space in range (n-1, 0, -1):
+    for space in range (0, n - row, -1):
         print(" ")
     for block in range (0, n):
         print("#")
