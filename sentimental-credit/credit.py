3a073d241ab4d3732298e9d6911f163049048fb2
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-02 00:18:28 +0000

    Thu, Nov 2, 2023, 12:18 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -8,8 +8,10 @@ if len(card_number) is not (13, 15, 16):
 # or for visa
 elif len(card_number) is 16 and card_number[0] + card_number[1] is (51, 52, 53, 54, 55):
     n = len(card_number)
+    list_of_numbers = []
     for i in range (n-1, 0, -2):
         card_number[i] = card_number[i] * 2
+        list_of_numbers.append(card_number[i])
 
 # or for american express
 
