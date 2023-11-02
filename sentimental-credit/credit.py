09304d11d0d94399e4c6cb3fe4e669632ff3695e
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-02 01:13:57 +0000

    Thu, Nov 2, 2023, 1:13 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -13,7 +13,7 @@ elif len(card_number) == 16 and card_number[0] == '5' and card_number[1] in ('1'
         digit = int(card_number[i])
         x = digit * 2
         if x > 9:
-            sum_of_numbers_1 += x/10 + x % 10
+            sum_of_numbers_1 += x//10 + x % 10
         else:
             sum_of_numbers_1 += x
 
@@ -33,7 +33,7 @@ elif len(card_number) == 15 and card_number[0] == '3' and card_number[1] in ('4'
         digit = int(card_number[i])
         x = digit * 2
         if x > 9:
-            sum_of_numbers_1 += x/10 + x % 10
+            sum_of_numbers_1 += x//10 + x % 10
         else:
             sum_of_numbers_1 += x
         print(f'{sum_of_numbers_1}')
@@ -42,6 +42,7 @@ elif len(card_number) == 15 and card_number[0] == '3' and card_number[1] in ('4'
     for i in range (n-1, 0, -2):
         digit = int(card_number[i])
         sum_of_numbers_2 += digit
+        print(f'{sum_of_numbers_2}')
 
     final_sum = sum_of_numbers_1 + sum_of_numbers_2
     if final_sum % 10 == 0:
