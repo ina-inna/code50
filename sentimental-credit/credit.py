97bc13fc45235269d102048a1709401f2a8625c5
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-02 11:10:44 +0000

    Thu, Nov 2, 2023, 11:10 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,6 +1,5 @@
 # prompt for user input
 def main():
-
     card_number = input("Number: ")
 
     # check if it's valid for mastercard
@@ -8,24 +7,29 @@ def main():
         print("INVALID")
 
     # or for mastercard
-    elif len(card_number) == 16 and card_number[0] == '5' and card_number[1] in ('1', '2', '3', '4', '5'):
-
+    elif (
+        len(card_number) == 16
+        and card_number[0] == "5"
+        and card_number[1] in ("1", "2", "3", "4", "5")
+    ):
         final_sum = calculate_sum(card_number)
         if final_sum % 10 == 0:
             print("MASTERCARD")
         else:
             print("INVALID")
 
-    elif len(card_number) == 15 and card_number[0] == '3' and card_number[1] in ('4', '7'):
-
+    elif (
+        len(card_number) == 15
+        and card_number[0] == "3"
+        and card_number[1] in ("4", "7")
+    ):
         final_sum = calculate_sum(card_number)
         if final_sum % 10 == 0:
             print("AMEX")
         else:
             print("INVALID")
 
-    elif len(card_number) in (13, 16) and card_number[0] == '4':
-
+    elif len(card_number) in (13, 16) and card_number[0] == "4":
         final_sum = calculate_sum(card_number)
         if final_sum % 10 == 0:
             print("VISA")
@@ -36,29 +40,30 @@ def main():
         print("INVALID")
 
 
-
 # function for sum calculation
+
 
 def calculate_sum(number):
     n = len(number)
     sum_of_numbers_1 = 0
-    for i in range (n-2, -1, -2):
+    for i in range(n - 2, -1, -2):
         digit = int(number[i])
         x = digit * 2
         if x > 9:
-            sum_of_numbers_1 += x//10 + x % 10
+            sum_of_numbers_1 += x // 10 + x % 10
         else:
             sum_of_numbers_1 += x
-        print(f'{sum_of_numbers_1}')
+        print(f"{sum_of_numbers_1}")
 
     sum_of_numbers_2 = 0
-    for i in range (n-1, -1, -2):
+    for i in range(n - 1, -1, -2):
         digit = int(number[i])
         sum_of_numbers_2 += digit
-        print(f'{sum_of_numbers_2}')
+        print(f"{sum_of_numbers_2}")
 
     result = sum_of_numbers_1 + sum_of_numbers_2
     return result
+
 
 main()
 
