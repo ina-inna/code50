ce83e57daee9d91d2987c4236323001c4ca93a57
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-02 01:00:25 +0000

    Thu, Nov 2, 2023, 1:00 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -9,26 +9,24 @@ if len(card_number) not in (13, 15, 16):
 elif len(card_number) == 16 and card_number[0] == '5' and card_number[1] in ('1', '2', '3', '4', '5'):
     n = len(card_number)
     sum_of_numbers_1 = 0
-    for i in range (n-2, 0, -2):
+    for i in range (n-2, -1, -2):
         digit = int(card_number[i])
         x = digit * 2
         if x > 9:
             sum_of_numbers_1 += x/10 + x % 10
         else:
             sum_of_numbers_1 += x
-        print (f"{sum_of_numbers_1}")
 
     sum_of_numbers_2 = 0
     for i in range (n-1, 0, -2):
         digit = int(card_number[i])
         sum_of_numbers_2 += digit
-        print (f"{sum_of_numbers_2}")
 
     final_sum = sum_of_numbers_1 + sum_of_numbers_2
     if final_sum % 10 == 0:
         print("MASTERCARD\n")
     else:
-        print("WTF\n")
+
 
 
 
