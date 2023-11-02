720e8ff6caab0bc07905abac5a6952dd00ed0bb3
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-02 12:14:26 +0000

    Thu, Nov 2, 2023, 12:14 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -37,7 +37,7 @@ def count_letters(user_text):
 def count_words(user_text):
     count = 1
     for i in range (len(user_text)):
-        if user_text[i] == ' ':
+        if user_text[i] == " " or user_text[i] == "I've":
            count += 1
     print(count)
     return count
