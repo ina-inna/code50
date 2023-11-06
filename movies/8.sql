cf721d0a3ea50d53e7b17f3834550f0d6db1d853
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-06 17:33:26 +0000

    Mon, Nov 6, 2023, 5:33 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,2 +1,1 @@
-SELECT name FROM people JOIN stars on people.id=stars.person_id
-JOIN movies ON stars.movie_id=movies.id WHERE title = 'Toy Story';
+SELECT name FROM people JOIN stars on people.id=person_id JOIN movies ON stars.movie_id=movies.id WHERE title = 'Toy Story';
