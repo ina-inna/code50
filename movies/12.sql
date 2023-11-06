49a2c2d8f8d051da1c598b905af64cdacb27bc07
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-06 18:10:47 +0000

    Mon, Nov 6, 2023, 6:10 PM +00:00

---
+++ movies/12.sql
@@ -0,0 +1,5 @@
+SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id
+   ...> JOIN people ON stars.person_id = people.id
+   ...> WHERE people.name = 'Bradley Cooper' AND movies.id IN
+   ...> (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id
+   ...> WHERE people.name = 'Jennifer Lawrence');
