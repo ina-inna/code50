a81de50a80d0ee8d8f58eff9e36ee435fd651c91
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-07 15:07:38 +0000

    Tue, Nov 7, 2023, 3:07 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -13,4 +13,9 @@ SELECT license_plate from bakery_security_logs
    ...> WHERE year = 2021 AND month = 7 AND day = 28
    ...> AND hour = 10 AND minute >=15 AND minute <=25;
 
+   -- list of suspects: retrieved license plates
+   SELECT name FROM people
+   ...> WHERE license_plate IN ('5P2BI95', '94KL13X', '6P58WS2', '4328GD8', 'G412CB7', 'L93JTIZ', '322W7JE', '0NTHK55');
+   
+
 
