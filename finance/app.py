37f443b84be6e5fb17602a5269041dca2999c664
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-24 17:30:45 +0000

    Fri, Nov 24, 2023, 5:30 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -132,8 +132,8 @@ def register():
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if len(rows) >= 1:
+            return apology("username already taken", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
