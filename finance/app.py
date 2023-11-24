99c4edcea31eebee3f9530be12acd890ec382146
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-24 19:32:39 +0000

    Fri, Nov 24, 2023, 7:32 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -104,7 +104,7 @@ def logout():
 def quote():
     """Get stock quote."""
     if request.method == "POST":
-        if not lookup("symbol"):
+        if not lookup(request.form.get("symbol")):
             return apology("stock symbol doesn't exist", 403)
         else:
             return render_template("quoted.html")
