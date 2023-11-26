c9f8fe609edd9a4d64085d5790f7e47f5d303825
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-26 12:18:52 +0000

    Sun, Nov 26, 2023, 12:18 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -36,9 +36,9 @@ def after_request(response):
 def index():
     """Show portfolio of stocks"""
 
-      user_stocks = db.execute("SELECT * FROM purchases WHERE id = ?", session.get("user_id"))
+      user_stocks = db.execute("SELECT * FROM purchases WHERE id = ? GROUP BY stock", session.get("user_id"))
         return render_template("index.html", user_stocks=user_stocks)
-    
+
     return apology("TODO")
 
 
