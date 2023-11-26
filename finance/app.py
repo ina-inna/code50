c1052e43ec2304a542f5de49c91eee8b6b941993
ina-inna <139999429+ina-inna@users.noreply.github.com>
2023-11-26 19:02:54 +0000

    Sun, Nov 26, 2023, 7:02 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -85,7 +85,7 @@ def buy():
             db.execute("INSERT INTO purchases (id_user, stock, number_shares, price_per_share, timestamp_column) VALUES(?, ?, ?, ?, CURRENT_TIMESTAMP)", session.get("user_id"), request.form.get("symbol"), int(request.form.get("shares")), stock["price"])
 
 
-        return redirect("/")
+        return redirect("index.html")
 
     else:
         return render_template("buy.html")
@@ -237,7 +237,7 @@ def sell():
             new_cash = user[0]["cash"] + cost
             db.execute("UPDATE users SET cash = ? WHERE id = ?", new_cash, session.get("user_id"))
 
-        return redirect("/")
+        return redirect("index.html")
 
 
     else:
